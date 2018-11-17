fbaeaa24146bcb73b769cf31d0c97ec53d1d1bfc
Ingo Wald <ingowald@gmail.com>
2018-11-17 11:57:08 -0700

    cmake improvement for better finding of optix on windows

--- cmake/FindOptiX.cmake
+++ cmake/FindOptiX.cmake
@@ -31,7 +31,18 @@
 # Our initial guess will be within the SDK.
 
 if (WIN32)
-  set(OptiX_INSTALL_DIR "C:/ProgramData/NVIDIA Corporation/OptiX SDK 5.1.0" CACHE PATH "Path to OptiX installed location.")
+#		set(OptiX_INSTALL_DIR "C:/ProgramData/NVIDIA Corporation/OptiX SDK 5.1.0" CACHE PATH "Path to OptiX installed location.")
+	find_path(searched_OptiX_INSTALL_DIR
+		NAME include/optix.h
+		PATHS
+		"C:/ProgramData/NVIDIA Corporation/OptiX SDK 5.1.1"
+		"C:/ProgramData/NVIDIA Corporation/OptiX SDK 5.1.0"
+		"C:/ProgramData/NVIDIA Corporation/OptiX SDK 5.0.1"
+		"C:/ProgramData/NVIDIA Corporation/OptiX SDK 5.0.0"
+		"C:/ProgramData/NVIDIA Corporation/OptiX SDK *"
+	)
+	mark_as_advanced(searched_OptiX_INSTALL_DIR)
+  set(OptiX_INSTALL_DIR ${searched_OptiX_INSTALL_DIR} CACHE PATH "Path to OptiX installed location.")
 else()
   set(OptiX_INSTALL_DIR $ENV{OptiX_INSTALL_DIR} CACHE PATH "Path to OptiX installed location.")
 endif()
