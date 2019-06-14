f513bc071cac74577235c5e78da6f0f9ee37e127
Trevor Black <trevortheblack@users.noreply.github.com>
2019-06-14 08:30:09 -0700

    Update checkered_texture.cu
    Slight error in checkered_texture.cu.
    Would lower the vertical frequency by 10.

--- OptiX-Path-Tracer/programs/textures/checkered_texture.cu
+++ OptiX-Path-Tracer/programs/textures/checkered_texture.cu
@@ -6,7 +6,7 @@ rtDeclareVariable(rtCallableProgramId<float3(float, float, float3, int)>, even,
                   , );
 
 RT_CALLABLE_PROGRAM float3 sample_texture(float u, float v, float3 p, int i) {
-  float sines = sin(10 * p.x) * sin(10 - p.y) * sin(10 * p.z);
+  float sines = sin(10 * p.x) * sin(10 * p.y) * sin(10 * p.z);
 
   if (sines < 0)
     return odd(u, v, p, 0);
